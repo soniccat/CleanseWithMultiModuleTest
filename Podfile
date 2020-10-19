72307ca36beddd77180fe2d048868625402b9c8e
Alexey Glushkov <soniccat@list.ru>
2020-10-19 10:51:12 +0300

    initial setup

---
+++ Podfile
@@ -0,0 +1,5 @@
+platform :ios, '11.0'
+
+target 'CleanseWithMultiModule' do
+  pod 'Cleanse', '4.2.6'
+end
---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CleanseWithMultiModule
+//
+//  Created by Alexey Glushkov on 19.10.2020.
+//  Copyright © 2020 Alexey Glushkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
